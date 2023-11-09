1d169a863e3cde5e47461b52cbf888d52f6e3510
domfournier <dominiquef@mirageoscience.com>
2023-11-09 13:41:32 -0800

    Update geoapps_utils/application/__init__.py

--- geoapps_utils/application/__init__.py
+++ geoapps_utils/application/__init__.py
@@ -2,5 +2,5 @@
 #
 #  This file is part of geoapps-utils.
 #
-#  geoapps is distributed under the terms and conditions of the MIT License
+#  geoapps-utils is distributed under the terms and conditions of the MIT License
 #  (see LICENSE file at the root of this source code package).
