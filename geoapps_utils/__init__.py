d6f28f89a33e5c7f4fb3de1e9cd17bb8250154c4
sebhmg <sebastienh@mirageoscience.com>
2023-12-20 13:45:34 -0500

    no need for assets_path function here

--- geoapps_utils/__init__.py
+++ geoapps_utils/__init__.py
@@ -7,18 +7,4 @@
 
 from __future__ import annotations
 
-from pathlib import Path
-
 __version__ = "0.2.0"
-
-
-def assets_path() -> Path:
-    """Return the path to the assets folder."""
-
-    parent = Path(__file__).parent
-    folder_name = f"{parent.name}-assets"
-    assets_folder = parent.parent / folder_name
-    if not assets_folder.is_dir():
-        raise RuntimeError(f"Assets folder not found: {assets_folder}")
-
-    return assets_folder
