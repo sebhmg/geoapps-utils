a2683a86e7aab9fabf106210520ee8cbfbbee974
sebhmg <sebastienh@mirageoscience.com>
2024-08-01 13:48:53 -0400

    need init to make assets a package

---
+++ geoapps_utils-assets/__init__.py
@@ -0,0 +1,6 @@
+#  Copyright (c) 2022-2024 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps-utils package.
+#
+#  geoapps-utils is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
